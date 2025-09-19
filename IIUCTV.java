78780fff6abe91f15d2dbb72d52a1dcf03f95df9
sazzadmain <sazzadhossainmm58@gmail.com>
2025-09-20 01:20:20 +0600

    Add files via upload

---
+++ IIUCTV.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class IIUCTV{
+    public static void main(String[] args){
+        int n,x;
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        x = sc.nextInt();
+        System.out.println("TotL COST: " + n*x);
+    }
+}
